9b1f65694672d980d8ce0654c509d0ceb3676802
ppdai-Roy <luojianhua@ppdai.com>
2017-03-28 15:43:38 +0800

    修复bug

---
+++ RoyFramework.framework/Headers/RoyFramework.h
@@ -0,0 +1,19 @@
+//
+//  RoyFramework.h
+//  RoyFramework
+//
+//  Created by Roy on 17/3/27.
+//  Copyright © 2017年 Roy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RoyFramework.
+FOUNDATION_EXPORT double RoyFrameworkVersionNumber;
+
+//! Project version string for RoyFramework.
+FOUNDATION_EXPORT const unsigned char RoyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoyFramework/PublicHeader.h>
+
+
